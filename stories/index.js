3507d1e9a08114e384c0f3a01038a626d603db18
dwcalade <dwcalade@yahoo.ca>
2022-05-17 17:58:07 -0700

    Styling the DayListItem

--- stories/index.js
+++ stories/index.js
@@ -2,6 +2,8 @@ import React from "react";
 
 import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
+import DayListItem from "components/DayListItem"
+
 
 import "index.scss";
 
@@ -13,7 +15,7 @@ storiesOf("Button", module)
   })
   .add("Base", () => <Button>Base</Button>)
   .add("Confirm", () => <Button confirm>Confirm</Button>)
-  .add("Danger", () => <Button danger>Cancel</Button>)
+  .add("Danger", () => <button class="button button--danger">Danger</button>)
   .add("Clickable", () => (
     <Button onClick={action("button-clicked")}>Clickable</Button>
   ))
@@ -21,4 +23,15 @@ storiesOf("Button", module)
     <Button disabled onClick={action("button-clicked")}>
       Disabled
     </Button>
+  ));
+
+  storiesOf("DayListItem", module) 
+  .addParameters({
+    backgrounds: [{ name: "dark", value: "#222f3e", default: true }]
+  })
+  .add("Unselected", () => <DayListItem name="Monday" spots={5} />)
+  .add("Selected", () => <DayListItem name="Monday" spots={5} selected />) 
+  .add("Full", () => <DayListItem name="Monday" spots={0} />)
+  .add("Clickable", () => (
+    <DayListItem name="Tuesday" setDay={action("setDay")} spots={5} /> 
   ));
