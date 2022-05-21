2f7af7581219590f4bec40d04937878273def9f2
dwcalade <dwcalade@yahoo.ca>
2022-05-20 22:00:37 -0700

    Using our Custom Hook

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -3,7 +3,9 @@ import React, { Fragment } from 'react';
 import Header from 'components/Appointment/Header';
 import Show from 'components/Appointment/Show';
 import Empty from 'components/Appointment/Empty';
+import Form from 'components/Appointment/Form';
 import 'components/Appointment/styles.scss';
+import useVisualMode from 'hooks/useVisualMode';
 
 export default function Appointment(props) {
   return (
