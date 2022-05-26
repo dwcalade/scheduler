8dcf7914689fc4a67702522becb139835e246526
dwcalade <dwcalade@yahoo.ca>
2022-05-25 23:07:01 -0700

    Added E2E Testing/Appointment

--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -1,6 +1,11 @@
 /* eslint-disable no-undef */
 describe("Navigation", () => {
-    it("should visit root", () => {
-      cy.visit("/");
-    });
-  }); 
+  it("should visit root", () => {
+    cy.visit("/");
+  });
+
+  it("Should navigate to Tuesday", () => {
+    cy.contains("[data-testid=day]", "Tuesday").click()
+      .should("have.class", "day-list__item--selected");
+  })
+}); 
